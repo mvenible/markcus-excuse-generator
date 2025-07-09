f84c1a79032d98c2561f9e6ba8c0a3f4c38ab02d
mvenible <mvenible@gmail.com>
2025-07-09 16:34:25 +0000

    new excuse generator

--- src/app.js
+++ src/app.js
@@ -8,4 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const excuse = getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when) + ".";
+  document.querySelector('#excuse').innerHTML = excuse;
 };
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying']
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
+
